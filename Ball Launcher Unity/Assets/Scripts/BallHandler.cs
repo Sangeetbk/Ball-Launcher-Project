e26c6ad3e7a8e52fe370e585ef3a0e7479aa1c22
sangeet <sangeetb1997@gmail.com>
2023-09-15 00:25:11 +0400

    V 3.0 - Fixing Ball Launch Mechanics

--- Ball Launcher Unity/Assets/Scripts/BallHandler.cs
+++ Ball Launcher Unity/Assets/Scripts/BallHandler.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,6 +11,10 @@ public class BallHandler : MonoBehaviour
     private Camera mainCamera;
     [SerializeField]
     private Rigidbody2D currentBall;
+    [SerializeField]
+    private SpringJoint2D currentSpringJoint;
+
+    private bool isDragging;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +25,36 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (currentBall == null) { return; }
+
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            
-            currentBall.isKinematic = false;
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
+
             return;
         }
-
+        
         currentBall.isKinematic = true;
         Vector2 mousePos =  Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPos = mainCamera .ScreenToWorldPoint(mousePos);
         currentBall.position = worldPos;
+        isDragging = true;
+    }
+
+    private void LaunchBall()
+    {
+        currentBall.isKinematic = false;
+        currentBall = null;
+        Invoke("DetachBall", 0.15f);
+    }
+
+    private void DetachBall()
+    {
+        currentSpringJoint.enabled = false;
+        currentSpringJoint = null;
     }
 }
